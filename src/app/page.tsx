bd9af437d5dbf568866c9beb3a2462fd6d5b649e
jjjiL0ng <qweqwe9412@gmail.com>
2025-05-14 19:37:36 +0900

    page view change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// import MainSpreadSheet from "@/components/MainSpreadSheet";
+import MainSpreadSheet from "@/components/MainSpreadSheet";
 import CSVChatComponent from "@/components/CSVChatComponent";
 import { CSVProvider } from "@/contexts/CSVContext";
 import ArtifactComponent from "@/components/ArtifactComponent";
@@ -7,28 +7,44 @@ export default function Home() {
   return (
     <CSVProvider>
       <div style={{ display: 'flex', width: '100%', height: '100vh' }}>
-        <div style={{
-          flex: '1',
-          width: '50%',
-          overflowY: 'auto',
-          overflowX: 'auto',
-          maxHeight: '100vh'
+        {/* 왼쪽 영역 (위아래로 분할) */}
+        <div style={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          width: '50%', 
+          height: '100%',
+          borderRight: '1px solid #ddd'
         }}>
-          {/* <MainSpreadSheet /> */}
-          <ArtifactComponent />
+          {/* 왼쪽 상단: MainSpreadSheet (2사분면) */}
+          <div style={{
+            height: '50%',
+            overflowY: 'auto',
+            overflowX: 'auto',
+            borderBottom: '1px solid #ddd'
+          }}>
+            <MainSpreadSheet />
+          </div>
+          
+          {/* 왼쪽 하단: CSVChatComponent (3사분면) */}
+          <div style={{
+            height: '50%',
+            overflowY: 'auto',
+            overflowX: 'auto'
+          }}>
+            <CSVChatComponent />
+          </div>
         </div>
+        
+        {/* 오른쪽 영역: ArtifactComponent (1사분면 및 4사분면 통합) */}
         <div style={{
-          flex: '1',
           width: '50%',
-          position: 'sticky',
-          top: 0,
-          height: '100vh',
-          overflowY: 'hidden'
+          height: '100%',
+          overflowY: 'auto',
+          overflowX: 'auto'
         }}>
-          <CSVChatComponent />
+          <ArtifactComponent />
         </div>
       </div>
     </CSVProvider>
-
   );
 }
