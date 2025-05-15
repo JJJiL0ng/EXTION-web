36281e4a164f35cbd02749ea323ea738b6c46c96
jjjiL0ng <qweqwe9412@gmail.com>
2025-05-15 19:36:32 +0900

    layout change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import MainSpreadSheet from "@/components/MainSpreadSheet";
 import CSVChatComponent from "@/components/CSVChatComponent";
-import ArtifactRenderContainer from "@/components/ArtifactRenderContainer";
 
 export default function Home() {
   return (
@@ -10,43 +9,25 @@ export default function Home() {
         height: '100vh', 
         overflow: 'hidden' // 페이지 전체 스크롤 방지
       }}>
-        {/* 왼쪽 영역 (위아래로 분할) */}
+        {/* 왼쪽 영역: MainSpreadSheet */}
         <div style={{ 
-          display: 'flex', 
-          flexDirection: 'column', 
           width: '50%', 
           height: '100%',
           borderRight: '1px solid #ddd',
-          overflow: 'hidden' // 왼쪽 영역 스크롤 방지
+          overflowY: 'auto',
+          overflowX: 'auto'
         }}>
-          {/* 왼쪽 상단: MainSpreadSheet (2사분면) */}
-          <div style={{
-            height: '50%',
-            overflowY: 'auto', // 컴포넌트 내부 스크롤 유지
-            overflowX: 'auto',
-            borderBottom: '1px solid #ddd'
-          }}>
-            <MainSpreadSheet />
-          </div>
-          
-          {/* 왼쪽 하단: CSVChatComponent (3사분면) */}
-          <div style={{
-            height: '50%',
-            overflowY: 'auto', // 컴포넌트 내부 스크롤 유지
-            overflowX: 'auto'
-          }}>
-            <CSVChatComponent />
-          </div>
+          <MainSpreadSheet />
         </div>
         
-        {/* 오른쪽 영역: ArtifactComponent (1사분면 및 4사분면 통합) */}
+        {/* 오른쪽 영역: CSVChatComponent */}
         <div style={{
           width: '50%',
           height: '100%',
-          overflowY: 'auto', // 컴포넌트 내부 스크롤 유지
+          overflowY: 'auto',
           overflowX: 'auto'
         }}>
-          <ArtifactRenderContainer />
+          <CSVChatComponent />
         </div>
       </div>
   );
