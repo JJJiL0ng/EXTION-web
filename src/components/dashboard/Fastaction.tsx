cd323f46de63b0d9e62fa5a5b5a7b9dd5376ddf1
jjjiL0ng <qweqwe9412@gmail.com>
2025-08-19 16:10:08 +0900

    lending v2 access allow fix

--- src/components/dashboard/Fastaction.tsx
+++ src/components/dashboard/Fastaction.tsx
@@ -4,6 +4,7 @@ import React, { useRef } from 'react';
 import { Upload, FileSpreadsheet } from 'lucide-react';
 import { useGenerateSpreadSheetId } from '../../_hooks/sheet/useGenerateSpreadSheetId';
 import { useGenerateChatId } from '../../_hooks/chat/useGenerateChatId';
+import Link from 'next/link';
 
 const FastActionButtons: React.FC = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -67,28 +68,22 @@ const FastActionButtons: React.FC = () => {
 
   return (
     <div className="grid grid-cols-2 gap-4 mx-auto">
-      {/* 숨겨진 파일 input */}
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={handleFileChange}
-        accept=".xlsx,.xls,.csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,text/csv"
-        style={{ display: 'none' }}
-      />
-      
       {/* 기존 엑셀 파일 업로드 버튼 */}
-      <button onClick={handleNewSheetClick} className="w-full bg-white border border-gray-200 hover:border-[#005de9] hover:bg-blue-50 transition-all duration-200 rounded-lg p-4 text-left group">
-        <div className="flex items-center space-x-3">
-          <div className="w-10 h-10 bg-white rounded-lg flex items-center justify-center group-hover:bg-gray-200 transition-colors">
-            <Upload className="w-7 h-7 text-[#005de9]" />
+      <Link href="/fileupload">
+        <button className="w-full bg-white border border-gray-200 hover:border-[#005de9] hover:bg-blue-50 transition-all duration-200 rounded-lg p-4 text-left group">
+          <div className="flex items-center space-x-3">
+            <div className="w-10 h-10 bg-white rounded-lg flex items-center justify-center group-hover:bg-gray-200 transition-colors">
+              <Upload className="w-7 h-7 text-[#005de9]" />
+            </div>
+            <div className="flex-1">
+              <span className="text-sm text-gray-900 group-hover:text-[#005de9] transition-colors">
+                <span className="font-semibold">기존 엑셀 파일 업로드</span> <br /> <span className="text-gray-500">기존의 csv, xlsx 파일을 업로드하여 ai와 작업하세요</span>
+              </span>
+            </div>
           </div>
-          <div className="flex-1">
-            <span className="text-sm text-gray-900 group-hover:text-[#005de9] transition-colors">
-              <span className="font-semibold">기존 엑셀 파일 업로드</span> <br /> <span className="text-gray-500">기존의 csv, xlsx 파일을 업로드하여 ai와 작업하세요</span>
-            </span>
-          </div>
-        </div>
-      </button>
+        </button>
+      </Link>
+
 
       {/* 새 작업 시트 생성 버튼 */}
       <button onClick={handleNewSheetClick} className="w-full bg-white border border-gray-200 hover:border-[#005de9] hover:bg-blue-50 transition-all duration-200 rounded-lg p-4 text-left group">
