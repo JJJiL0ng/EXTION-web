148e21adfa0fd92149461fcacd409ce80ad8c33c
jjjiL0ng <qweqwe9412@gmail.com>
2025-09-29 13:38:06 +0900

    build error fix

--- src/app/(minimal)/testCompo/page.tsx
+++ src/app/(minimal)/testCompo/page.tsx
@@ -1,18 +1,5 @@
-// "use client";
+"use client";
 
-// import React, {useMemo} from "react";
-
-// import FileUploadModal from "@/_aa_superRefactor/compo/shared/FileUploadModal";
-// import { SpreadsheetProvider } from "@/_contexts/SpreadsheetContext";
-
-// export default function TestCompoPage() {
-//     const spreadRef = useMemo(() => ({ current: null }), []);
-
-//     return (
-//         <SpreadsheetProvider spreadRef={spreadRef}>
-
-//             <FileUploadModal isOpen={true} userId="guest_739a77a7-e872-4322-9c2f-b7d7117b7d1f" />
-//         </SpreadsheetProvider>
-
-//     )
-// }
+export default function TestCompoPage() {
+    return <div>TestCompoPage - 테스트 컴포넌트 페이지</div>;
+}
