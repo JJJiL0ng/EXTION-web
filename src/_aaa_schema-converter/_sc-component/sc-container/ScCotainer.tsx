7ad3dcfddb74fa25d944b6bf6faa166f446b0503
JJJiL0ng <qweqwe9412@gmail.com>
2025-10-15 19:54:57 +0900

    component fix

--- src/_aaa_schema-converter/_sc-component/sc-container/ScCotainer.tsx
+++ src/_aaa_schema-converter/_sc-component/sc-container/ScCotainer.tsx
@@ -3,19 +3,33 @@ import React, { useMemo } from "react";
 import DualSpreadViewer from "../sc-sheet/DualSpreadViewer";
 import { TargetSheetProvider } from "@/_aaa_schema-converter/_sc-context/TargetSheetProvider";
 import { SourceSheetProvider } from "@/_aaa_schema-converter/_sc-context/SourceSheetProvider";
+import { MappingTopBar } from "../sc-sheet/MappingTopBar";
 
 export default function ScContainer() {
     const spreadRefSourceSheet = useMemo(() => ({ current: null }), []);
     const spreadRefTargetSheet = useMemo(() => ({ current: null }), []);
 
+    // 매핑 시작 핸들러
+    const handleStartMapping = () => {
+        // TODO: 여기에 실제 매핑 로직을 구현하세요
+        console.log('매핑 프로세스 시작...');
+        // 예: API 호출, 상태 업데이트 등
+    };
+
     return (
-        <TargetSheetProvider spreadRef={spreadRefTargetSheet}>
-            <SourceSheetProvider spreadRef={spreadRefSourceSheet}>
-                <DualSpreadViewer
-                    spreadRefSourceSheet={spreadRefSourceSheet}
-                    spreadRefTargetSheet={spreadRefTargetSheet}
-                />
-            </SourceSheetProvider>
-        </TargetSheetProvider>
+        <div className="flex flex-col h-screen w-screen">
+            {/* 매핑 정보 및 시작 버튼 탭바 */}
+            <MappingTopBar onStartMapping={handleStartMapping} />
+            
+            {/* 듀얼 스프레드시트 뷰어 */}
+            <TargetSheetProvider spreadRef={spreadRefTargetSheet}>
+                <SourceSheetProvider spreadRef={spreadRefSourceSheet}>
+                    <DualSpreadViewer
+                        spreadRefSourceSheet={spreadRefSourceSheet}
+                        spreadRefTargetSheet={spreadRefTargetSheet}
+                    />
+                </SourceSheetProvider>
+            </TargetSheetProvider>
+        </div>
     );
 }
